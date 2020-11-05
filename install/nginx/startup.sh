e54e93be95d3b72d3700dda51579df4128457676
LinShiX <3002452711>
2020-11-05 15:26:57 +0800

    nginx 加入两个ssl文件，保证能执行脚本写入

--- install/nginx/startup.sh
+++ install/nginx/startup.sh
@@ -5,12 +5,12 @@ if [ ! -f /etc/nginx/ssl/default.crt ]; then
     openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
     openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
 fi
-
-cat /var/run/crond.pid
-crontab -l
 # Start crond in background
-if [ ! -f /var/run/crond.pid ]; then
-  /etc/init.d/cron reload
-fi
+#
+#cat /var/run/crond.pid
+#crontab -l
+#if [ ! -f /var/run/crond.pid ]; then
+#  /etc/init.d/cron reload
+#fi
 # Start nginx in foreground
 nginx
